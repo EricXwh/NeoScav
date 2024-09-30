90a76af81b3da8572df5b173e1ec18b229ace83c
EricXwh <ericxiewh@163.com>
2024-09-30 04:39:20 -0400

    change movement method

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,69 +1,62 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    [Header("Movment Setting")]
-    public float moveSpeed = 5f;
+    public float moveSpeed = 5f;            
+    public float rotationSpeed = 10f;       
+    public float jumpHeight = 2f;           
+    public float gravity = -9.81f;          
 
-    [Header("Jump Setting")]
-    public float jumpForce = 20f; 
-    public Transform groundCheck;
-    public float groundDistance = 0.2f;
-    public LayerMask groundMask;
-
-    [Header("Gravity Setting")]
-    public float gravityMultiplier = 2.5f; 
-    public float fallMultiplier = 2.5f;    
-
-    private Rigidbody rb;
-    private Vector3 movement;
+    // 私有变量
+    private CharacterController controller;
+    private Vector3 velocity;
     private bool isGrounded;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        rb.constraints = RigidbodyConstraints.FreezeRotation; 
+        controller = GetComponent<CharacterController>();
     }
 
     void Update()
     {
+        HandleMovement();
+        HandleJump();
+    }
+
+    void HandleMovement()
+    {
+        isGrounded = controller.isGrounded;
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f; 
+        }
+
         float moveX = Input.GetAxis("Horizontal");
-        float moveZ = Input.GetAxis("Vertical");
+        float moveZ = Input.GetAxis("Vertical"); 
 
-        movement = new Vector3(moveX, 0f, moveZ).normalized * moveSpeed;
+        Vector3 move = new Vector3(moveX, 0, moveZ).normalized;
 
+        if (move.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.Euler(0f, targetAngle, 0f);
 
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+            Vector3 moveDirection = transform.forward * move.magnitude;
+            controller.Move(moveDirection * moveSpeed * Time.deltaTime);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void HandleJump()
+    {
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z); 
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
-    }
-
-    void FixedUpdate()
-    {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-
-        if (rb.velocity.y < 0)
-        {
-            rb.AddForce(Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * rb.mass, ForceMode.Acceleration);
-        }
-        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
-        {
-            rb.AddForce(Vector3.up * Physics.gravity.y * (gravityMultiplier - 1) * rb.mass, ForceMode.Acceleration);
-        }
-
-        Vector3 newPosition = rb.position + new Vector3(movement.x, 0f, movement.z) * Time.fixedDeltaTime;
-        rb.MovePosition(newPosition);
-    }
-
-    void OnDrawGizmosSelected()
-    {
-        if (groundCheck == null)
-            return;
-
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(groundCheck.position, groundDistance);
     }
 }
